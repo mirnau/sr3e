13c684fc2304b484739d6efdacf2ddd6b2eaad86
Mikael Bernau <mikael.bernau@gmail.com>
2025-07-31 23:32:47 +0200

    Simplification of asset less

--- module/svelte/apps/components/AssetManager/InventoryCard.svelte
+++ module/svelte/apps/components/AssetManager/InventoryCard.svelte
@@ -31,19 +31,15 @@
       if (!item?.uuid) return;
       const payload = {
          type: item.documentName ?? "Item",
-         uuid: item.uuid
+         uuid: item.uuid,
       };
       event.dataTransfer?.setData("text/plain", JSON.stringify(payload));
-      if (event.currentTarget instanceof HTMLElement)
-         event.dataTransfer.setDragImage(event.currentTarget, 16, 16);
+      if (event.currentTarget instanceof HTMLElement) event.dataTransfer.setDragImage(event.currentTarget, 16, 16);
    }
 </script>
 
-<div
-   class="asset-card"
-   draggable="true"
-   ondragstart={onDragStart}
->
+<!-- svelte-ignore a11y_no_static_element_interactions -->
+<div class="asset-card" draggable="true" ondragstart={onDragStart}>
    <div class="asset-background-layer"></div>
    <div class="image-mask">
       <img src={item.img} role="presentation" alt={item.name} />
@@ -52,14 +48,14 @@
       <div class="asset-card-row">
          <div class="asset-card-column">
             <h3 class="no-margin uppercase">{item.name}</h3>
-            <h4 class="no-margin uppercase">{item.name}</h4>
+            <h3 class="no-margin uppercase">Skill : {item.name}</h3>
+            {#if item.type === "weapon"}
+               <h4 class="no-margin uppercase">¥ {item.system.commodity.cost} - {item.system.mode}</h4>
+            {/if}
          </div>
       </div>
       <div class="asset-card-row">
-         <button
-            class="sr3e-toolbar-button fa-solid fa-dice"
-            aria-label="Roll"
-            onclick={() => console.log("Roll")}
+         <button class="sr3e-toolbar-button fa-solid fa-dice" aria-label="Roll" onclick={() => console.log("Roll")}
          ></button>
          <button
             class="sr3e-toolbar-button fa-solid fa-pencil"
