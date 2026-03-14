f73e5bdf7755e101f20cf618379193464b30311b
Mikael Bernau <79576604+mirnau@users.noreply.github.com>
2026-03-14 19:29:30 +0100

    wip(ts-migration): story teller screen

--- module/ui/actors/StorytellerScreenApp.svelte
+++ module/ui/actors/StorytellerScreenApp.svelte
@@ -1,13 +1,18 @@
 <script lang="ts">
-  import KarmaManager from "./storyteller-screen/KarmaManager.svelte";
-  import TimeManager from "./storyteller-screen/TimeManager.svelte";
+   import SheetCard from "../common-components/SheetCard.svelte";
+   import KarmaManager from "./storyteller-screen/KarmaManager.svelte";
+   import TimeManager from "./storyteller-screen/TimeManager.svelte";
 </script>
 
 <div class="storyteller-screen">
-  <div class="storyteller-screen__panel storyteller-screen__panel--primary">
-    <KarmaManager />
-  </div>
-  <div class="storyteller-screen__panel">
-    <TimeManager />
-  </div>
+   <SheetCard>
+      <div class="storyteller-screen__panel storyteller-screen__panel--primary">
+         <KarmaManager />
+      </div>
+   </SheetCard>
+   <SheetCard>
+      <div class="storyteller-screen__panel">
+         <TimeManager />
+      </div>
+   </SheetCard>
 </div>
